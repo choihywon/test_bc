324372999c04c9dc2919809400a867af187bbeef
choihyewon <dongsim00@gmail.com>
2024-11-21 15:48:18 +0900

    20241121

---
+++ SortingExample/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        int[] array = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            array[i] = Integer.parseInt(input[i]);
+        }
+
+        int[] selectionSortedArray = array.clone();
+        selectionSort(selectionSortedArray);
+
+        scanner.close();
+    }
+
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+
+
+
+
+    private static void selectionSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+        printArray(array);
+    }
+
+}
